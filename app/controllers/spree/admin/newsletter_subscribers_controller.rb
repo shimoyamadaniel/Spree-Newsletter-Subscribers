32587640672178ff384c06ee981109666a203ff8
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-05-14 09:51:27 -0300

    adding paginate admin index

--- app/controllers/spree/admin/newsletter_subscribers_controller.rb
+++ app/controllers/spree/admin/newsletter_subscribers_controller.rb
@@ -1,6 +1,11 @@
 module Spree
   module Admin
     class NewsletterSubscribersController < ResourceController
+	
+		def index
+			@newsletter_subscribers = Spree::NewsletterSubscriber.paginate
+		end
+	
     end
   end
 end
